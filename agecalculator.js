90850032634df7ef3458642cabe91cf3b76c01ae
Tyler Robertson <tylerr@tnrdesign.net>
2017-09-21 14:49:11 +0000

    Adds conditionals exercises

---
+++ agecalculator.js
@@ -0,0 +1,5 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+console.log(ageCalculator("Rose", 1968, 2017));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
---
+++ icecreamflavours.js
@@ -0,0 +1,6 @@
+// Do not modify these first two lines
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry", "root beer"];
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length)
